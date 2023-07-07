1d0b01765750b57a2b119607d3c973c43a45e400
anrichtait <anrichjtait@gmail.com>
2023-07-07 23:16:30 +0200

    Added code for testing

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 
 /**
-*
+* main - multiplies 2 numbers
+* @argc: number of args
+* @argv: args
+* Return: 1 if error, 0 if pass
 */
 
 int main(int argc, char *argv[])
 {
 	if (argc < 1)
-		{
-			printf("Error\n");
-			return (1);
-		}
+	{
+		printf("Error\n");
+		return (1);
+	}
 	else
-		{
+	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
