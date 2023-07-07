bd72a7ea99d72c5e3f84d3028607a3884ab91b77
anrichtait <anrichjtait@gmail.com>
2023-07-07 23:23:03 +0200

    Added code for testing

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 /**
-*
+* main - calculates change for a value
+* @argc: number of arguments
+* @argv: args
+* Return: values...
 */
 
+int main(int argc, char *argv[])
+{
 
+	return (1);
+}
