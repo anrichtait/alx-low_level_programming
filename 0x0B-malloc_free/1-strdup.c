dc81fa8f43871d7d17cebcc491db56adbcd24a0e
anrichtait <anrichjtait@gmail.com>
2023-07-10 09:25:11 +0200

    Added code for testing

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+* *_strdup - copies a string
+* @str: string to copy
+* Return: NULL if fail and ptr to new string if success
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *newStr = malloc(sizeof(char) * strlen(str));
+
+	if (newStr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		newStr[i] = str[i];
+	}
+	return (newStr);
+}
