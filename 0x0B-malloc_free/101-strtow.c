22e7badd75a53eaf55465b759744a6a0b5b09c4d
anrichtait <anrichjtait@gmail.com>
2023-07-11 11:53:42 +0200

    added code for testing

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* **strtow - splits a string into words
+* @str: string to process
+* Return: NULL or Pointer
+*/
+
+char **strtow(char *str)
+{
+	char s1, s2, s3, s4;
+	int i;
+
+	if (str == NULL || (strcmp(str, "") == 0))
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(str); i++)
+	{
+	}
+	return (NULL);
+}
