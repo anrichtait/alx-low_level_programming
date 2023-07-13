11d300a8189ca3fbcadf476f75f2205aa9b22e5b
anrichtait <anrichjtait@gmail.com>
2023-07-13 10:27:17 +0200

    Added code for final testing

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* *_calloc - allocates memory for an array
+* @nmemb: size of array to allocate for
+* @size: size of array data type to allocate
+* Return: NULL if any passed value is 0 or if malloc fails, else a ptr to mem
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *array;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	return (array);
+}
